0528b7cf582498e06f4fe1aa7c2e7ca60dd470a1
Denis Polunosik <yneskladovae27@gmail.com>
2022-01-04 00:07:16 +0300

    feat: add 'Wilson primes'

---
+++ 8 kyu/Wilson primes.js
@@ -0,0 +1,15 @@
+/*
+Wilson primes satisfy the following condition. Let P represent a prime number.
+
+Then ((P-1)! + 1) / (P * P) should give a whole number.
+
+Your task is to create a function that returns true if the given number is a Wilson prime.
+*/
+
+function amIWilson(p) {
+    function f(n) {
+      return n <= 1 ? 1 : n * f(n - 1);
+    }
+    
+    return (f(p - 1) + 1) / (p * p) % 1 === 0;
+}
