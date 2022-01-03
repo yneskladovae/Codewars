944f65cab81ab679ad18ed1732654aefdd9f1215
Denis Polunosik <yneskladovae27@gmail.com>
2022-01-03 22:41:35 +0300

    fix: change 'Rock Paper Scissors' task

--- 8 kyu/Rock Paper Scissors.js
+++ 8 kyu/Rock Paper Scissors.js
@@ -1,15 +1,20 @@
 /*
-Thinkful - Logic Drills: Traffic light
+Rock Paper Scissors
 
-You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+Let's play! You have to return which player won! In case of a draw return Draw!.
 
-Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
-
-For example, update_light('green') should return 'yellow'.
+Examples:
+rps('scissors','paper') // Player 1 won!
+rps('scissors','rock') // Player 2 won!
+rps('paper','paper') // Draw!
 */
 
-function updateLight(current) {
-    if (current === "green") return "yellow";
-    if (current === "yellow") return "red";
-    if (current === "red") return "green";
-}
+const rps = (p1, p2) => {
+    if (p1 === p2) return "Draw!"
+    if (p1 === 'rock' && p2 === 'scissors' || p1 === 'scissors' && p2 === 'paper' || p1 === 'paper' && p2 === 'rock'){
+      return 'Player 1 won!';
+    }
+  
+    return 'Player 2 won!';
+  
+};
