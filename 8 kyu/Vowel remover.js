2abd91ca5ced036ee54ef7646911ed5fb8a9fbf8
Denis Polunosik <yneskladovae27@gmail.com>
2022-01-04 22:05:45 +0300

    feat: add 'Vowel remover'

---
+++ 8 kyu/Vowel remover.js
@@ -0,0 +1,15 @@
+/*
+Create a function called shortcut to remove all the lowercase vowels in a given string.
+
+Examples
+"hello"     -->  "hll"
+"codewars"  -->  "cdwrs"
+"goodbye"   -->  "gdby"
+"HELLO"     -->  "HELLO"
+
+Don't worry about uppercase vowels.
+*/
+
+function shortcut(string){
+  return string.replace(/[aeiou]/g, '');
+}
