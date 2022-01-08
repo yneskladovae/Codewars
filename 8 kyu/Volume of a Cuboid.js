9b46cf6bad364ac84cbe684ee3d78ad49bf4310d
Denis Polunosik <yneskladovae27@gmail.com>
2022-01-08 22:32:00 +0300

    feat: add 'Volume of a Cuboid' task

---
+++ 8 kyu/Volume of a Cuboid.js
@@ -0,0 +1,7 @@
+/* 
+Bob needs a fast way to calculate the volume of a cuboid with three values: length, width and the height of the cuboid. Write a function to help Bob with this calculation.
+*/
+
+function getVolumeOfCuboid(length, width, height) {
+  return length * width * height;
+}
