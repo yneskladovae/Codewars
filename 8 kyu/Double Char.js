c70e11d364f9784875ba071f4afbe4cbd7bfd602
Denis Polunosik <yneskladovae27@gmail.com>
2022-02-20 01:10:02 +0300

    feat: add tasks

---
+++ 8 kyu/Double Char.js
@@ -0,0 +1,13 @@
+/* 
+Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+doubleChar("String") ==> "SSttrriinngg"
+
+doubleChar("Hello World") ==> "HHeelllloo  WWoorrlldd"
+
+doubleChar("1234!_ ") ==> "11223344!!__  "
+*/
+
+function doubleChar(str) {
+    return str.split('').map((i) => i + i).join('');
+}
