80332b64954478225cc8d3478d92e412f54c295c
Denis Polunosik <yneskladovae27@gmail.com>
2022-02-22 21:53:31 +0300

    feat: add tasks

---
+++ 8 kyu/Is there a vowel in there.js
@@ -0,0 +1,19 @@
+/* 
+Given an array of numbers, check if any of the numbers are the character codes for lower case vowels (a, e, i, o, u).
+
+If they are, change the array value to a string of that vowel.
+
+Return the resulting array.
+*/
+
+function isVow(a){
+  const vowels = {
+    97: 'a',
+    101: 'e',
+    105: 'i',
+    111: 'o',
+    117: 'u',
+  }
+  
+  return a.map( n => vowels[n] ? vowels[n] : n);
+}
