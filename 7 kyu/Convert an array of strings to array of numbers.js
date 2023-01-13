c8a3b213919fced8bee27a530a25a9b31e550e71
yneskladovae <yneskladovae27@gmail.com>
2023-01-13 17:49:57 +0300

    docs: change description

--- 7 kyu/Convert an array of strings to array of numbers.js
+++ 7 kyu/Convert an array of strings to array of numbers.js
@@ -1,6 +1,5 @@
 /* 
 Some really funny web dev gave you a sequence of numbers from his API response as an sequence of strings!
-
 You need to cast the whole array to the correct type.
 
 Create the function that takes as a parameter a sequence of numbers represented as strings and outputs a sequence of numbers.
