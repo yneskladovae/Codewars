bcb6ab0c5b74e6fdd0dd2a2316cd48d9f72995f1
yneskladovae <yneskladovae27@gmail.com>
2023-01-14 15:26:09 +0300

    docs: change desc

--- 7 kyu/max diff - easy.js
+++ 7 kyu/max diff - easy.js
@@ -1,5 +1,6 @@
 /* 
 You must implement a function that returns the difference between the largest and the smallest value in a given list / array (lst) received as the parameter.
+
 lst contains integers, that means it may contain some negative numbers
 if lst is empty or contains a single element, return 0
 lst is not sorted
