ddc96cdcd9395ed3936a1a9e0415f1453b57f6d3
yneskladovae <yneskladovae27@gmail.com>
2023-01-15 16:38:15 +0300

    feat: add task

---
+++ 7 kyu/Divide and Conquer.js
@@ -0,0 +1,16 @@
+/* 
+Given a mixed array of number and string representations of integers, add up the non-string integers and subtract this from the total of the string integers.
+
+Return as a number.
+*/
+
+function divCon(x) {
+  let numSum = 0;
+  let strSum = 0;
+
+  x.map((a) => {
+    typeof a == "number" ? (numSum += a) : (strSum += +a);
+  });
+
+  return numSum - strSum;
+}
