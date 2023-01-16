e8deac74a24db600c95bb31f4f60f84cf6390942
yneskladovae <yneskladovae27@gmail.com>
2023-01-16 13:08:51 +0300

    change desc

--- 8 kyu/Sum without highest and lowest number.js
+++ 8 kyu/Sum without highest and lowest number.js
@@ -1,6 +1,5 @@
 /* 
 Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
-
 The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
 
 Mind the input validation.
