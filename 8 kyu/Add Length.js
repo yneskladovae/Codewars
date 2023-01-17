6a04d5a27f54844a8c4a72e2b40551b8b1555414
yneskladovae <yneskladovae27@gmail.com>
2023-01-17 17:50:17 +0300

    change desc

--- 8 kyu/Add Length.js
+++ 8 kyu/Add Length.js
@@ -1,5 +1,6 @@
 /* 
 What if we need the length of the words separated by a space to be added at the end of that same word and have it returned as an array?
+
 Example(Input --> Output)
 
 "apple ban" --> ["apple 5", "ban 3"]
