1279b759df38a16f116b552c015c8939d3d8d498
yneskladovae <yneskladovae27@gmail.com>
2023-02-09 22:25:30 +0300

    added tasks

--- 6 kyu/Multiples of 3 or 5.js
+++ 6 kyu/Multiples of 3 or 5.js
@@ -15,3 +15,63 @@ function solution(number) {
   }
   return sum;
 }
+
+// function test(){
+// 	const result = Array.from(elements);
+// result.reverse()
+// return result;
+// }
+// test();
+// let elements = document.querySelectorAll('.item');
+// const result = Array.from(elements);
+// result.reverse()
+// console.log(result); // => [div#root, div.page-wrapper.document-page, ...]
+
+// const timePartBlock = document.querySelector('.umatch-matches grid-style');
+
+//         for (i = 0; i < result.length; i++) {
+
+// 			timePartBlock.innerHTML = `${result[i]}`;  data += '<div class="reserv__date" >' + dateDay + ' ' + dateMonth + '</div>';
+
+//             }
+
+let parent = document.querySelector("#parent");
+
+let p = document.createElement("p");
+p.innerHTML = "!";
+
+parent.appendChild(p);
+
+let elements = document.querySelectorAll(".item");
+const cont = document.querySelector(".umatch-matches grid-style");
+const elementDiv = document.createElement("div");
+elementDiv.classList.add("test");
+cont.appendChild(elementDiv);
+const elDiv = documentquerySelector(".test");
+const result = Array.from(elements);
+result.reverse();
+
+function test1() {
+  for (i = 0; i < result.length; i++) {
+    elDiv.innerHTML = `${result[i]}`;
+  }
+}
+test1();
+
+window.onload = () => {
+  let elements = document.querySelectorAll(".item");
+  const container = document.querySelector(".umatch-matches");
+  const elementDiv = document.createElement("div");
+  elementDiv.classList.add("newdiv");
+  container.appendChild(elementDiv);
+  const elDiv = document.querySelector(".newdiv");
+  const result = Array.from(elements);
+  result.reverse();
+
+  function test1() {
+    for (i = 0; i < elements.length; i++) {
+      elDiv.innerHTML = `${result[i].outerHTML}`;
+    }
+  }
+  test1();
+};
